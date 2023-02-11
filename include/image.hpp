ac32b90770aa8ba017bada71a120da2c8324059a
JustKidding <jk@vin.ovh>
2023-02-11 06:15:29 -0500

    add license header and fix formatting

--- include/image.hpp
+++ include/image.hpp
@@ -1,3 +1,19 @@
+// Display images inside a terminal
+// Copyright (C) 2023  JustKidding
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef __IMAGE__
 #define __IMAGE__
 
@@ -7,8 +23,8 @@
 class Image
 {
 public:
-    static std::unique_ptr<Image> load(const std::string& filename,
-            const int& max_width, const int& max_height);
+    static auto load(const std::string& filename, int max_width, int max_height)
+        -> std::unique_ptr<Image>;
     virtual ~Image() {}
 
     virtual auto width() -> int = 0;
--- src/image/libvips.cpp
+++ src/image/libvips.cpp
@@ -1,3 +1,19 @@
+// Display images inside a terminal
+// Copyright (C) 2023  JustKidding
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "libvips.hpp"
 
 using namespace vips;
@@ -15,7 +31,8 @@ max_height(max_height)
         // thumbnail not required
         img = check.colourspace(VIPS_INTERPRETATION_sRGB);
     } else {
-        img = VImage::thumbnail(filename.c_str(), max_width - 1).colourspace(VIPS_INTERPRETATION_sRGB);
+        img = VImage::thumbnail(filename.c_str(), max_width - 1)
+            .colourspace(VIPS_INTERPRETATION_sRGB);
     }
     // alpha channel required
     if (!img.has_alpha()) img = img.bandjoin(255);
