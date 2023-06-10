75d44c125941219866dfb54f9eb55bd783e7330c
JustKidding <jk@vin.ovh>
2023-06-10 06:58:16 -0500

    generate tar and dsc file

--- .github/workflows/obs-upload.sh
+++ .github/workflows/obs-upload.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
+set -e
 
-ls -l third_party/cli11
+DATETIME=$(date -R)
+VERSION=$(grep "project(" CMakeLists.txt | grep -oP '\(\K[^\)]+' | cut -f 6 -d ' ')
+TARFILE="ueberzugpp_${VERSION}.tar.xz"
+DSCFILE="ueberzugpp_${VERSION}.dsc"
+
+sed -e "s;@@VERSION@@;${VERSION};g" -e "s;@@DATETIME@@;${DATETIME};g" < debian/changelog.in > debian/changelog
+
+tar cfJ "$TARFILE" ./*
+
+cat > "$DSCFILE" << EOF
+Format: 3.0 (native)
+Source: ueberzugpp
+Binary: ueberzugpp
+Architecture: any
+Version: $VERSION
+Maintainer: JustKidding <jk@vin.ovh>
+Homepage: https://github.com/jstkdng/ueberzugpp
+Standards-Version: 4.5.1
+Build-Depends: $(sed -n '/^Build-Depends:/,/^$/p' < debian/control | tr -d '\n' | cut -f 2 -d : | sed -r 's;^ *| *$;;g')
+Package-List:
+ ueberzugpp deb misc optional arch=any
+Checksums-Sha1:
+ $(sha1sum < "$TARFILE" | cut -f 1 -d ' ') $(stat -c '%s' "$TARFILE") $TARFILE
+Checksums-Sha256:
+ $(sha256sum < "$TARFILE" | cut -f 1 -d ' ') $(stat -c '%s' "$TARFILE") $TARFILE
+Files:
+ $(md5sum < "$TARFILE" | cut -f 1 -d ' ') $(stat -c '%s' "$TARFILE") $TARFILE
+EOF
+
+cat "$DSCFILE"
